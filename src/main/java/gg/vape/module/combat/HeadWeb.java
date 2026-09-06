d3a097ddc94dfb0537a1f7a59f2d0572cd0c3b02
Dodster <dodster@example.com>
2026-09-06 03:18:44 +0300

    HeadWeb: remove center punch, place instantly with high-speed aim, use +3 block offset

--- src/main/java/gg/vape/module/combat/HeadWeb.java
+++ src/main/java/gg/vape/module/combat/HeadWeb.java
@@ -1,6 +1,7 @@
 package gg.vape.module.combat;
 
 import gg.vape.Vape;
+import gg.vape.config.ClientSettings;
 import gg.vape.event.EventHandler;
 import gg.vape.event.impl.EventPreTick;
 import gg.vape.input.AttackKeyController;
@@ -12,7 +13,6 @@ import gg.vape.module.utility.clutch.ClutchPlacementPathUtils;
 import gg.vape.module.utility.clutch.PlacementTarget;
 import gg.vape.notification.NotificationType;
 import gg.vape.rotation.AdaptiveRotationController;
-import gg.vape.rotation.RotationAngles;
 import gg.vape.rotation.RotationControlClaim;
 import gg.vape.rotation.RotationManager;
 import gg.vape.utils.BlockUtil;
@@ -33,7 +33,6 @@ import gg.vape.wrapper.impl.RayTraceResult;
 import gg.vape.wrapper.impl.Vec3;
 import gg.vape.wrapper.impl.Vec3i;
 import gg.vape.wrapper.impl.World;
-import gg.vape.config.ClientSettings;
 
 public class HeadWeb
 extends UtilityMod {
@@ -41,21 +40,14 @@ extends UtilityMod {
     private static final int STATE_ATTACK = 1;
     private static final int STATE_WAIT = 2;
     private static final int STATE_PLACE = 3;
-    private static final int STATE_PUNCH = 4;
-    private static final int PUNCH_PHASE_FLICKING = 0;
-    private static final int PUNCH_PHASE_ATTACKING = 1;
-    private static final int PUNCH_PHASE_DONE = 2;
+    private static final float PLACEMENT_AIM_SPEED = 200.0f;
 
     private final BooleanValue silentAim = BooleanValue.create(this, "Silent aim", true,
             "Aims without moving the camera when placing the web");
-    private final NumberValue aimSpeed = NumberValue.create(this, "Aim speed", "#.#", "", 1.0, 5.0, 10.0, 0.1,
-            "Speed of the aim when placing the web");
     private final NumberValue sprintDelay = NumberValue.create(this, "Sprint delay", "#", "ticks", 0.0, 0.0, 10.0, 1.0,
             "How long to force sprint before attacking");
     private final NumberValue webDelay = NumberValue.create(this, "Web delay", "#", "ticks", 0.0, 0.0, 30.0, 1.0,
             "How long to wait after the sprint hit before placing the web");
-    private final BooleanValue centerPunch = BooleanValue.create(this, "Center punch", false,
-            "Extra hit flicked off-angle (like HitFlick) to redirect knockback toward the center of the web block");
 
     private final RotationControlClaim rotationClaim = SharedModuleControlClaims.rotation;
 
@@ -65,10 +57,6 @@ extends UtilityMod {
     private int cobwebSlot = -1;
     private boolean releasePending;
     private EntityOtherPlayerMP target;
-    private int punchPhase;
-    private boolean punchAttacked;
-    private float punchFlickYaw;
-    private AdaptiveRotationController punchRotationController;
     private int placeX;
     private int placeY;
     private int placeZ;
@@ -81,8 +69,8 @@ extends UtilityMod {
 
     public HeadWeb() {
         super("HeadWeb", Category.COMBAT,
-                "Sprint hits a target and places a web one block above their head");
-        this.addValue(this.silentAim, this.aimSpeed, this.sprintDelay, this.webDelay, this.centerPunch);
+                "Sprint hits a target and places a web three blocks above their feet");
+        this.addValue(this.silentAim, this.sprintDelay, this.webDelay);
         this.rotationClaim.setPriority(this, 6);
     }
 
@@ -96,8 +84,8 @@ extends UtilityMod {
         this.originalSlot = player.V$src$Lgg_vape_wrapper_impl_InventoryPlayer_$erqak6().v();
         this.cobwebSlot = this.findHotbarSlot(player.V$src$Lgg_vape_wrapper_impl_InventoryPlayer_$erqak6(), "cobweb");
         if (this.cobwebSlot < 0) {
-            NotificationType notificationType = NotificationType.WARNING;
-            Vape.INSTANCE.getNotificationManager().show("HeadWeb", "Cobweb not in hotbar", notificationType, 3000L);
+            Vape.INSTANCE.getNotificationManager().show("HeadWeb", "Cobweb not in hotbar", NotificationType.WARNING,
+                    3000L);
             this.setEnabled(false, true);
             return;
         }
@@ -110,10 +98,6 @@ extends UtilityMod {
         }
         this.ticks = 0;
         this.releasePending = false;
-        this.punchPhase = PUNCH_PHASE_FLICKING;
-        this.punchAttacked = false;
-        this.punchFlickYaw = 0.0f;
-        this.punchRotationController = null;
         this.placeStarted = false;
         this.webClicked = false;
         this.webRotationController = null;
@@ -147,10 +131,6 @@ extends UtilityMod {
                 this.tickWait();
                 break;
             }
-            case STATE_PUNCH: {
-                this.tickPunch(player);
-                break;
-            }
             case STATE_PLACE: {
                 this.tickPlace(player);
                 break;
@@ -169,7 +149,7 @@ extends UtilityMod {
 
     private void tickAttack(EntityPlayerSP player) {
         this.placeX = MathUtil.floor(this.target.z());
-        this.placeY = MathUtil.floor(this.target.N()) + 2;
+        this.placeY = MathUtil.floor(this.target.N()) + 3;
         this.placeZ = MathUtil.floor(this.target.h());
         this.releasePending = AttackKeyController.requestSyntheticAttack(this);
         this.releaseForcedKeys(player);
@@ -186,112 +166,7 @@ extends UtilityMod {
             return;
         }
         this.ticks = 0;
-        if (this.centerPunch.getEffectiveValue().booleanValue()) {
-            this.startPunch();
-            return;
-        }
         this.state = STATE_PLACE;
-    }
-
-    private void startPunch() {
-        EntityPlayerSP player = Minecraft.thePlayer();
-        if (player.isNull() || this.target == null || this.target.isNull()) {
-            this.finish();
-            return;
-        }
-        this.punchFlickYaw = this.computePunchFlickYaw();
-        this.punchPhase = PUNCH_PHASE_FLICKING;
-        this.punchAttacked = false;
-        this.punchRotationController = null;
-        this.ticks = 0;
-        this.state = STATE_PUNCH;
-    }
-
-    private float computePunchFlickYaw() {
-        EntityPlayerSP player = Minecraft.thePlayer();
-        double enemyX = this.target.z();
-        double enemyZ = this.target.h();
-        float yawToEnemy = (float)(Math.atan2(-(enemyX - player.z()), enemyZ - player.h()) * 57.29577951308232);
-        double centerX = (double)this.placeX + 0.5;
-        double centerZ = (double)this.placeZ + 0.5;
-        float yawToCenter = (float)(Math.atan2(-(centerX - enemyX), centerZ - enemyZ) * 57.29577951308232);
-        return MathUtil.wrapAngleTo180(yawToCenter - yawToEnemy);
-    }
-
-    private void tickPunch(EntityPlayerSP player) {
-        if (this.target == null || this.target.isNull()) {
-            this.finish();
-            return;
-        }
-        if (!this.rotationClaim.isOwnedBy(this) && !this.rotationClaim.acquire(this, true)) {
-            this.finish();
-            return;
-        }
-        if (this.punchRotationController == null) {
-            AdaptiveRotationController controller = new AdaptiveRotationController(
-                    Vec3.create(this.target.z(), this.target.N() + 1.0, this.target.h()));
-            controller.setNormalizeTargetYaw(false);
-            controller.setRetainAfterCompletion(true);
-            controller.setClampStepToRemaining(true);
-            controller.setTolerance(0.0f);
-            controller.setScaleAxesProportionally(false);
-            controller.setLinearAcceleration(false);
-            controller.setRelativeMode(false);
-            controller.setSpeed(((Double)this.aimSpeed.getValue()).floatValue());
-            controller.setYawOffset(this.punchFlickYaw);
-            this.punchRotationController = controller;
-        }
-        Vec3 punchAimPoint = Vec3.create(this.target.z(), this.target.N() + 1.0, this.target.h());
-        this.punchRotationController.setTarget(punchAimPoint);
-        this.punchRotationController.setYawOffset(this.punchFlickYaw);
-        this.punchRotationController.setSpeed(((Double)this.aimSpeed.getValue()).floatValue());
-        if (!this.punchRotationController.equals(RotationManager.INSTANCE.getActiveController())) {
-            RotationManager.INSTANCE.setController(this.punchRotationController);
-        }
-        switch (this.punchPhase) {
-            case PUNCH_PHASE_FLICKING: {
-                if (this.hasReachedPunchRotation() || ++this.ticks >= 12) {
-                    this.ticks = 0;
-                    this.punchPhase = PUNCH_PHASE_ATTACKING;
-                }
-                break;
-            }
-            case PUNCH_PHASE_ATTACKING: {
-                if (!this.punchAttacked) {
-                    this.punchAttacked = true;
-                    this.releasePending = AttackKeyController.requestSyntheticAttack(this);
-                }
-                if (++this.ticks >= 2) {
-                    this.punchPhase = PUNCH_PHASE_DONE;
-                }
-                break;
-            }
-            case PUNCH_PHASE_DONE: {
-                if (this.punchRotationController != null) {
-                    RotationManager.INSTANCE.releaseController(this.punchRotationController);
-                }
-                this.punchRotationController = null;
-                this.punchAttacked = false;
-                this.ticks = 0;
-                this.state = STATE_PLACE;
-                break;
-            }
-        }
-    }
-
-    private boolean hasReachedPunchRotation() {
-        if (this.punchRotationController == null) {
-            return false;
-        }
-        RotationAngles rotationAngles = this.punchRotationController.calculateRotation(
-                Vec3.create(this.target.z(), this.target.N() + 1.0, this.target.h()));
-        if (rotationAngles == null) {
-            return false;
-        }
-        float offsetYaw = rotationAngles.getYaw() + this.punchFlickYaw;
-        float yawError = Math.abs(MathUtil.wrapAngleTo180(RotationManager.INSTANCE.getManagedYaw() - offsetYaw));
-        float pitchError = Math.abs(MathUtil.wrapAngleTo180(RotationManager.INSTANCE.getManagedPitch() - rotationAngles.getPitch()));
-        return Math.max(yawError, pitchError) <= 6.0f;
     }
 
     private void tickPlace(EntityPlayerSP player) {
@@ -307,13 +182,13 @@ extends UtilityMod {
             return;
         }
         if (this.webRotationController == null || !this.webRotationController.isComplete()) {
-            if (++this.ticks > 60) {
+            if (++this.ticks > 15) {
                 this.finish();
             }
             return;
         }
         if (!this.isWebRayTraceValid()) {
-            if (++this.ticks > 20) {
+            if (++this.ticks > 10) {
                 this.finish();
             }
             return;
@@ -379,18 +254,14 @@ extends UtilityMod {
             controller.setRetainAfterCompletion(true);
             controller.setClampStepToRemaining(true);
             controller.setTolerance(0.1f);
-            controller.setAngleBasedAcceleration(true);
-            controller.setScaleAxesProportionally(true);
-            controller.setLinearAcceleration(true);
-            controller.setCubicAcceleration(true);
             controller.setRelativeMode(false);
-            controller.setSpeed(((Double)this.aimSpeed.getValue()).floatValue());
+            controller.setSpeed(PLACEMENT_AIM_SPEED);
             this.webRotationController = controller;
         }
         if (this.webRotationController instanceof AdaptiveRotationController) {
             ((AdaptiveRotationController)this.webRotationController).setTarget(this.webAimPoint);
         }
-        this.webRotationController.setSpeed(((Double)this.aimSpeed.getValue()).floatValue());
+        this.webRotationController.setSpeed(PLACEMENT_AIM_SPEED);
         if (!this.webRotationController.equals(RotationManager.INSTANCE.getActiveController())) {
             RotationManager.INSTANCE.setController(this.webRotationController);
         }
@@ -509,12 +380,8 @@ extends UtilityMod {
         if (this.webRotationController != null) {
             RotationManager.INSTANCE.releaseController(this.webRotationController);
         }
-        if (this.punchRotationController != null) {
-            RotationManager.INSTANCE.releaseController(this.punchRotationController);
-        }
         this.rotationClaim.release(this);
         this.webRotationController = null;
-        this.punchRotationController = null;
         this.target = null;
         this.webSupportBlock = null;
         this.webFacing = null;
@@ -540,12 +407,8 @@ extends UtilityMod {
         if (this.webRotationController != null) {
             RotationManager.INSTANCE.releaseController(this.webRotationController);
         }
-        if (this.punchRotationController != null) {
-            RotationManager.INSTANCE.releaseController(this.punchRotationController);
-        }
         this.rotationClaim.release(this);
         this.webRotationController = null;
-        this.punchRotationController = null;
         this.target = null;
         this.webSupportBlock = null;
         this.webFacing = null;
